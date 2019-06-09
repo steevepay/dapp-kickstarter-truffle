6aea921df0af83e91de70206f1aa9fcbec1cdca4
Steeve Payraudeau <steeve.payraudeau@epitech.eu>
2019-06-09 17:00:37 +0400

    .gitignore fix

--- .next/dist/routes.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var routes = require('next-routes');
-
-module.exports = routes().add('/campaigns/new', '/campaign/new').add('/campaigns/:address', '/campaign/show').add('/campaigns/:address/requests', '/campaign/requests/index').add('/campaigns/:address/requests/new', '/campaign/requests/new');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwibW9kdWxlIiwiZXhwb3J0cyIsImFkZCJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBVCxBQUFOOztBQUVBLE9BQU8sQUFBUCxVQUFpQixTQUNoQixBQURnQixJQUNaLEFBRFksa0JBQ00sQUFETixpQkFFaEIsQUFGZ0IsSUFFWixBQUZZLHVCQUVXLEFBRlgsa0JBR2hCLEFBSGdCLElBR1osQUFIWSxnQ0FHb0IsQUFIcEIsNEJBSWhCLEFBSmdCLElBSVosQUFKWSxvQ0FJd0IsQUFKeEIsQUFBakIiLCJmaWxlIjoicm91dGVzLmpzIiwic291cmNlUm9vdCI6Ii9wcml2YXRlL3RtcC9raWNrc3RhcnQtdHJ1ZmZsZSJ9
